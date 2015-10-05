08fea3bec297e8ecdbe816af8a4465277b1056fd
Peter Lada <peter@easypairings.com>
2015-10-05 13:43:32 -0400

    Initial Commit

---
+++ stackview-exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackview-exercise
+//
+//  Created by Peter Lada on 10/5/15.
+//  Copyright © 2015 Peter Lada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
